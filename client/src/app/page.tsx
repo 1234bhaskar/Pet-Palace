a5071053e83ba49296c71361f1e71984733dfefc
1234bhaskar <bhaskar2021chand@gmail.com>
2024-08-12 23:09:26 +0530

    Categories Image Component Added in Home Page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client "
 
+import { CarouselPlugin } from "@/components/Carosel";
+import {Categories} from "@/components/HomePage/Categories";
 import { Dashboard } from "@/components/navbarcom/testing";
 
 
@@ -8,7 +10,10 @@ export default function Home() {
   return (
     <>
       <div className="h-10"><Dashboard/></div>
-      <p className="text-black mt-10">hello </p>
+      <div className="w-screen mt-6 sm:flex sm:visible invisible justify-center "><CarouselPlugin/></div>
+      <div>
+        <Categories/>
+      </div>
     </>
   );
 }
