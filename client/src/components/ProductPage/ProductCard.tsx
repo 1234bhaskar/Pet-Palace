0895330fb1bf70bdae0492ec2810e86e2053a10a
1234bhaskar <bhaskar2021chand@gmail.com>
2024-08-23 17:24:33 +0530

    fetching all products using useGetAllProducts on frontend

--- client/src/components/ProductPage/ProductCard.tsx
+++ client/src/components/ProductPage/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 import {
   Card,
   CardContent,
@@ -8,23 +8,31 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import Image from 'next/image'
-export default function ProductCard() {
+import Link from 'next/link'
+import { Product } from '../../../gql/graphql'
+
+interface ProductCardProp{
+  product:Product
+}
+export const ProductCard:FC<ProductCardProp>=(props)=> {
+
+  const {product}=props;
   return (
-    <div className=' place-self-center my-5'>
+      <Link href={"/product/"+product.id}  className=' place-self-center my-5'>
    <Card>
   <CardHeader>
     <CardDescription>
-        <Image className='' src={"https://cdn.shopify.com/s/files/1/0565/8021/0861/products/Frame2_13_1.jpg?v=1696580837"} alt='product' width={200} height={10}/>
+        <Image className='' src={product.images?.[0] as string} alt='product' width={200} height={10}/>
     </CardDescription>
   </CardHeader>
   <CardContent>
-    <p>{"collar"}</p>
+    <p>{product.name}</p>
   </CardContent>
   <CardFooter>
-    <p>${"30"}</p>
+    <p>${product.price}</p>
   </CardFooter>
 </Card>
 
-    </div>
+</Link>
   )
 }
